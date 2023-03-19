2de43253891f43aaf5f0edffd0ec6c2375415b67
Swathi Bhat S <swathibhat9836@gmail.com>
2023-03-19 21:26:46 +0530

    Add files via upload

---
+++ BubbleSort.c
@@ -0,0 +1,35 @@
+//C program to sort elements using Bubble Sort
+
+#include<stdio.h>
+
+void main()
+{
+    int a[100],n,i,j,temp;
+
+    printf("Enter the number of elements to be sorted: ");
+    scanf("%d",&n);
+
+    printf("Enter %d elements\n",n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+
+    //sorting
+    for(i=0;i<n-1;i++)
+    {
+        for(j=0;j<n-i-1;j++)
+        {
+            if(a[j]>a[j+1])
+            {
+                temp=a[j];
+                a[j]=a[j+1];
+                a[j+1]=temp;
+            }
+        }
+    }
+    printf("Sorted list in ascending order\n");
+
+    for(i=0;i<n;i++)
+        printf("%d\t",a[i]);
+}
